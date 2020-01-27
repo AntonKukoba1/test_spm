59b7fd237c8f1e04031094d3e944dc3e8c35b557
Anton Kukoba <kukoba.anton@apriorit.com>
2020-01-27 17:40:20 +0200

    added acp.swift

---
+++ Code/ACP.swift
@@ -0,0 +1,20 @@
+//
+//  SampleClass.swift
+//  SwiftPackageSample
+//
+//  Created by Cristian Ortega on 18/12/2019.
+//  Copyright © 2019 SEAT. All rights reserved.
+//
+
+import Foundation
+
+/// Sample class
+public class MySampleClass {
+    // MARK: - Initializers
+    
+    /// Initializes an instance of this class
+    public init() {
+        // Do whatever you need here
+    }
+
+}
